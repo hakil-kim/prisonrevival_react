106954b52515d584d12da3ca87541d409c1f3bc7
Hakil Kim <hakil.kim@gmail.com>
2026-05-20 10:45:07 +0900

    feat: add VS Code auto start dev server task and enable polling in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // 소스 수정 시 자동 반영(HMR)을 100% 보장하는 폴링 설정
+    }
+  }
 })
